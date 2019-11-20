9d81a5ac020417f8a1e276d2f780320a78562984
Angshuman Mazumdar <54448615+amazumda23@users.noreply.github.com>
2019-11-20 17:06:16 -0500

    Added scripts

---
+++ Bipolar_Final/Assets/Environment.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Environment : MonoBehaviour
+{
+    //public Transform center;
+    public GameObject pathParent;
+    public GameObject pathMesh;
+    //public GameObject trees;
+    public GameObject player;
+    public Rigidbody playerRigidBody;
+
+    GameObject closestCornerForPath;
+    public float distToNextCorner;
+
+    public bool shouldChangeCorner;
+    public float speed;
+    public Vector3 distBW;
+    // Start is called before the first frame update
+
+    //private void Awake()
+    //{
+    //    closestCornerForPath = player.GetComponent<PlayerScript>().closestCornerPrefab;
+    //}
+
+    void Start()
+    {
+        shouldChangeCorner = false;
+        playerRigidBody = player.GetComponent<Rigidbody>();
+
+        speed = 1.0f;
+
+        //player.transform.position = pathParent.transform.position;
+        //player.transform.forward = pathMesh.transform.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //closestCornerForPath = player.GetComponent<PlayerScript>().closestCornerPrefab;
+        //distToNextCorner = player.GetComponent<PlayerScript>().distBetween;
+        //pathParent.transform.LookAt(center);
+    //}
+
+    //private void OnTriggerStay(Collider other)
+    //{
+        //while(player.transform.forward == pathMesh.transform.forward)
+        {
+            pathParent.transform.RotateAround(Vector3.zero, Vector3.down, 10 * Time.deltaTime);
+
+            pathMesh.transform.position += pathMesh.transform.forward * Time.deltaTime * speed;
+            //trees.transform.position = pathMesh.transform.position;
+            //pathMesh.transform.position = new Vector3(pathMesh.transform.position.x, 0, pathMesh.transform.position.z);
+
+            shouldChangeCorner = ChangeCorner();
+
+            if (shouldChangeCorner)
+            {
+                print("Here");
+                //pathParent.transform.position = Vector3.Slerp(pathParent.transform.position, closestCornerForPath.transform.position, Time.deltaTime * speed);
+                //pathParent.transform.localRotation = Quaternion.FromToRotation(pathParent.transform.forward, closestCornerForPath.transform.forward);
+
+                //pathParent.transform.RotateAround(Vector3.zero, Vector3.up, 45.0f);
+
+                pathParent.transform.position = Vector3.Slerp(pathParent.transform.position, closestCornerForPath.transform.position, Time.deltaTime);
+            }
+        }
+
+        calcAngle();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        pathParent.transform.forward = player.transform.forward;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        pathParent.transform.forward = player.transform.forward;
+
+    }
+
+    public bool ChangeCorner()
+    {
+        if(distToNextCorner < 0.3f)
+        {
+            print("Changing to next corner!");
+            return true;
+        }
+        return false;
+    }
+
+    public Vector3 calcAngle()
+    {
+        //distBW = pathMesh.transform.position - player.transform.position;
+        distBW = distBW.normalized;
+
+        //float dot = Vector3.Dot(distBW, transform.forward);
+        //float angle = Mathf.Acos(dot) * Mathf.Rad2Deg;
+
+        print(distBW);
+
+        return distBW;
+
+
+    }
+
+}
---
+++ Bipolar_Final/Assets/PlayerScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Transform[] corners;
+    public float distBetween;
+
+    public Transform closestCorner;
+    public GameObject closestCornerPrefab;
+
+    Color lineColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        closestCorner = null;
+        lineColor = new Color(50, 30, 25);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Transform cornerPos in corners)
+        {
+            closestCorner = GetClosestEdge(corners);
+        }
+        closestCornerPrefab = closestCorner.gameObject;
+        Debug.DrawLine(transform.position, closestCorner.transform.position, Color.red);
+
+        
+    }
+
+    Transform GetClosestEdge(Transform[] edges)
+    {
+        Transform bestTarget = null;
+        float closestDistanceSqr = Mathf.Infinity;
+        Vector3 currentPosition = transform.position;
+        foreach (Transform potentialTarget in edges)
+        {
+            Vector3 directionToTarget = potentialTarget.position - currentPosition;
+            float dSqrToTarget = directionToTarget.sqrMagnitude;
+            if (dSqrToTarget < closestDistanceSqr)
+            {
+                closestDistanceSqr = dSqrToTarget;
+                //print(closestDistanceSqr);
+                bestTarget = potentialTarget;
+            }
+        }
+
+        return bestTarget;
+    }
+}
