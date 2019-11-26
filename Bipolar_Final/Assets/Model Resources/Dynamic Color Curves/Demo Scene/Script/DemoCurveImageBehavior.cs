a2a5d11764dd0bb45fb904fc49e522b624a2eda5
Angshuman Mazumdar <54448615+amazumda23@users.noreply.github.com>
2019-11-25 20:05:42 -0500

    Added new files (updated colliders)

---
+++ Bipolar_Final/Assets/Model Resources/Dynamic Color Curves/Demo Scene/Script/DemoCurveImageBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.ImageEffects;
+
+namespace com.cortastudios.DynamicColorCorrection.DemoScene
+{
+    [RequireComponent(typeof(Image))]
+    ///There are for images in the demo scene showing the active color curves affecting it.
+    ///This script controls the transparency of those images (which, in turn, clue the user on
+    ///how much each curve is affecting the scene, in realtime).
+    ///This script is for the demo scene only and probably have no use on any other project in which
+    ///Dynamic Color Correction is used.
+    public class DemoCurveImageBehavior : MonoBehaviour
+    {
+        public bool InverseTransparency;
+        public bool IsDepthCurve;
+        private Image ImageComponent;
+
+        void Start()
+        {
+            ImageComponent = GetComponent<Image>();
+            if (IsDepthCurve && GameObject.FindGameObjectWithTag("MainCamera").GetComponent<ColorCorrectionCurves>().mode == ColorCorrectionCurves.ColorCorrectionMode.Simple)
+                ImageComponent.enabled = false;
+        }
+
+        public void SetTransparency(float transparency)
+        {
+            var newColor = ImageComponent.color;
+            if (InverseTransparency)
+                transparency = 1 - transparency;
+            newColor.a = transparency;
+            ImageComponent.color = newColor;
+        }
+    }
+}
---
+++ Bipolar_Final/Assets/Scripts/CreateGizmo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateGizmo : MonoBehaviour
+{
+    public Transform corner;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        foreach(Transform child in corner)
+        {
+            Gizmos.DrawSphere(child.position, 0.1f);
+        }
+
+    }
+}
